3c05677a4cb854b6eac743297d231faae776aca5
telescope_user <telescope_user@Telescopes-MacBook-Pro-4.local>
2017-04-07 21:53:36 -0700

    updated keys.js and madison.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'JKssxNIZ88ck16F6DmjVIWoKu',
+  consumer_secret: 'Cm0JD6Y0ziUM8TBRldGGRBYCIJosht7VWhFe8STG67D4DkC0Pi',
+  access_token_key: '114837892-Em6ewwiAmIeRnH2vUGJayYEDPeCYmpZg5spuGMqn',
+  access_token_secret: 'roVQrWatHgPxK5ZhGUVGlG8Vw7sORL46yDSIUg7rk0mlb',
 }
